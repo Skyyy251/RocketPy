b5005ef0089199bcc861ac6257a4a8ae7aa6d714
Skyyy251 <lukas_lein@web.de>
2026-08-13 13:04:21 +0200

    another one

--- rocketpy/environment/magnetic_field/constant_magnetic_field.py
+++ rocketpy/environment/magnetic_field/constant_magnetic_field.py
@@ -8,7 +8,7 @@ class ConstantMagneticField(MagneticFieldModel):  #
     """Constant magnetic field model"""
 
     def __init__(self, field):
-        self.field = np.asarray(field, dtype=format)
+        self.field = np.asarray(field, dtype=float)
 
         if self.field.shape != (3,):
             raise ValueError("Magnetic field must be a 3-element vector.")
